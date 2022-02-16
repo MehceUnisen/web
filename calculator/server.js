acd916048343717c20795a9e97155750aa4d0254
CursedIndividual <mehceu@gmail.com>
2022-02-16 23:25:06 +0300

    basic expressJs stuff covered

---
+++ calculator/server.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/html/index.html");
+});
+
+app.post("/", function(req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    res.send("the result " + (num1 + num2));
+});
+
+app.listen(3000);
