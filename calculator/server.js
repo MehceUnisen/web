485c4632fadc88b51378c8a281e51a98874ff17d
CursedIndividual <mehceu@gmail.com>
2022-02-16 23:37:08 +0300

    made somewhat calculator

--- calculator/server.js
+++ calculator/server.js
@@ -10,10 +10,20 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/html/index.html");
 });
 
-app.post("/", function(req, res) {
+app.get("/mycalc", function(req, res) {
+    res.sendFile(__dirname + "/html/my-calculator.html")
+});
+// app.post("/", function(req, res) {
+//     var num1 = Number(req.body.num1);
+//     var num2 = Number(req.body.num2);
+//     res.send("the result " + (num1 + num2));
+// });
+
+app.post("/mycalc", function(req, res) {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
-    res.send("the result " + (num1 + num2));
+    res.send("result is " + (num1 * num2));
+
 });
 
 app.listen(3000);
