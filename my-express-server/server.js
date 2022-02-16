146e60dd953429002ea76dcbc67d8413fca2fd96
CursedIndividual <mehceu@gmail.com>
2022-02-16 22:53:12 +0300

    covered the basics for express

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -8,6 +8,14 @@ app.get("/", function(req, res) {
     res.send();
 });
 
+app.get("/contact", function(req, res) {
+    res.send("you've reached");
+});
+
+app.get("/about", function(req, res) {
+    res.send("I'm so horny lol that's all about me");
+})
+
 app.listen(3000, function() {
     console.log("server started on port 3000");
 });
