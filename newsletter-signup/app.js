be9d355afe2645493b83c1edbc3e925ae9c87590
MehceUnisen <mehceu@gmail.com>
2022-03-19 16:12:40 +0300

    unfinished email signup project

---
+++ newsletter-signup/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const parser = require("body-parser");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var mailAddress = req.body.email;
+
+    console.log(firstName + lastName + mailAddress);
+});
+
+app.listen(3000, function() {
+    console.log("listening on port 3000");
+});
